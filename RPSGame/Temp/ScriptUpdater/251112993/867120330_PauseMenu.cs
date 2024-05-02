db9115fafbd170e7a74328088dbce82c647fc8a9
nthn22 <nathanzamora45@gmail.com>
2024-05-02 01:28:18 -0700

    created a pause menu
    accessible by pressing "ESC" on keyboard.

---
+++ RPSGame/Temp/ScriptUpdater/251112993/867120330_PauseMenu.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool Paused = false;
+    public GameObject PauseMenuCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(Paused)
+            {
+                Play();
+            }
+            else{
+                Stop();
+            }
+        }
+    }
+
+    void Stop()
+    {
+        PauseMenuCanvas.SetActive(true);
+        Time.timeScale = 0;
+        Paused = true;
+        GetComponent<AudioSource>().volume = 0;
+    }
+
+    public void Play()
+    {
+        PauseMenuCanvas.SetActive(false);
+        Time.timeScale = 1f;
+        Paused = false;
+        GetComponent<AudioSource>().volume = 1;
+    }
+
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+
+    }
+}
