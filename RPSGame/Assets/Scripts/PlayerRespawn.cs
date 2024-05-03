779145cacb49afdbd29b9b583b921f67c9ec6ce3
Solares10 <solaresmichelle22@gmail.com>
2024-05-02 17:01:06 -0700

    Fixing AudioRespawn

--- RPSGame/Assets/Scripts/PlayerRespawn.cs
+++ RPSGame/Assets/Scripts/PlayerRespawn.cs
@@ -1,14 +1,61 @@
 using UnityEngine;
+
 public class PlayerRespawn : MonoBehaviour
 {
-   // AudioManager audioManager;
+    AudioManager audioManager;
     private Vector3 spawnLocation; // Store the spawn location
-  /*  private void Awake()
+    private bool isRespawning = false; // Flag to check if the player is respawning
+
+    private void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+    }
+
+    void Start()
+    {
+        // Set the initial spawn location to the player's starting position
+        spawnLocation = transform.position;
+    }
+
+    public void SetSpawnLocation(Vector3 location)
+    {
+        // Set the spawn location to the specified location
+        spawnLocation = location;
+    }
+
+    public void Respawn()
+    {
+        // Check if the player is respawning
+        if (!isRespawning)
+        {
+            // Play the respawn audio
+            audioManager.PlaySFX(audioManager.respawn);
+        }
+
+        // Respawn the player at the spawn location
+        transform.position = spawnLocation;
+
+        // Set the flag to true to indicate the player is respawning
+        isRespawning = true;
+    }
+
+    // Method to reset the respawn flag (call this when the respawn animation/audio is finished)
+    public void ResetRespawnFlag()
+    {
+        isRespawning = false;
+    }
+}
+/*using UnityEngine;
+public class PlayerRespawn : MonoBehaviour
+{
+   AudioManager audioManager;
+    private Vector3 spawnLocation; // Store the spawn location
+   private void Awake()
     {
         audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
         audioManager.PlaySFX(audioManager.respawn);
     }
-*/
+
     void Start()
     {
         // Set the initial spawn location to the player's starting position
@@ -25,3 +72,4 @@ public class PlayerRespawn : MonoBehaviour
         transform.position = spawnLocation;
     }
 }
+*/
